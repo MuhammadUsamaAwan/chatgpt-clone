f2d052418bd22410765d111c6d6de6fe35cf96eb
Muhammad Usama <underusama@gmail.com>
2023-02-11 20:17:02 +0500

    setup next auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,27 @@
 import './globals.css';
 import Sidebar from '@/components/Sidebar';
+import SessionProvider from '@/components/SessionProvider';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextAuth]';
+import Login from '@/components/Login';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang='en'>
       <head />
       <body>
-        <main className='flex'>
-          <Sidebar />
-          <div className='flex-1 bg-[#343541]'>{children}</div>
-        </main>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <main className='flex'>
+              <Sidebar />
+              <div className='flex-1 bg-[#343541]'>{children}</div>
+            </main>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
