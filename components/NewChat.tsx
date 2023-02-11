0ed1691d1c18342d96db118d384b8b656bbde073
Muhammad Usama <underusama@gmail.com>
2023-02-11 22:35:20 +0500

    create new chat

---
+++ components/NewChat.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { useState } from 'react';
+import { BsPlus } from 'react-icons/bs';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '@/firebase';
+
+export default function NewChat() {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const createNewChat = async () => {
+    setIsLoading(true);
+    const doc = await addDoc(collection(db, 'users', session?.user?.email!, 'chats'), {
+      messages: [],
+      userId: session?.user?.email!,
+      createdAt: serverTimestamp(),
+    });
+    setIsLoading(false);
+    router.push(`/chat/${doc.id}`);
+  };
+
+  return (
+    <button
+      className='mx-2 flex items-center space-x-3 rounded-md border border-white/20 p-3 duration-200 ease-out hover:bg-gray-500/10'
+      onClick={createNewChat}
+      disabled={isLoading}
+    >
+      <BsPlus className='text-base' />
+      <span className='text-sm'>New Chat</span>
+    </button>
+  );
+}
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,28 +1,13 @@
 'use client';
-import { BsPlus } from 'react-icons/bs';
 import { MdLogout } from 'react-icons/md';
 import { FiTrash2 } from 'react-icons/fi';
-import { signOut, useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { addDoc, collection } from 'firebase/firestore';
-import { db } from '@/firebase';
+import { signOut } from 'next-auth/react';
+import NewChat from './NewChat';
 
 export default function Sidebar() {
-  const router = useRouter();
-  const { data: session } = useSession();
-
-  const createNewChat = async () => {};
-
   return (
     <div className='flex h-screen flex-col space-y-1 bg-[#202123] py-2 md:min-w-[16rem]'>
-      {/* new chat */}
-      <button
-        className='mx-2 flex items-center space-x-3 rounded-md border border-white/20 p-3 duration-200 ease-out hover:bg-gray-500/10'
-        onClick={createNewChat}
-      >
-        <BsPlus className='text-base' />
-        <span className='text-sm'>New Chat</span>
-      </button>
+      <NewChat />
       {/* chats */}
       <div className='flex-1 space-y-1 overflow-y-auto border-b border-white/20 px-2'></div>
       {/* clear conversation */}
