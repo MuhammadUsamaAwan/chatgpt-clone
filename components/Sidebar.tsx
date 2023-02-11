c5667c3a73dccb53c634d1fb04da41841d5129a9
Muhammad Usama <underusama@gmail.com>
2023-02-11 22:08:55 +0500

    custom scrollbar and firestore

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,20 +2,31 @@
 import { BsPlus } from 'react-icons/bs';
 import { MdLogout } from 'react-icons/md';
 import { FiTrash2 } from 'react-icons/fi';
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '@/firebase';
 
 export default function Sidebar() {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const createNewChat = async () => {};
+
   return (
-    <div className='flex h-screen flex-col space-y-1 overflow-y-auto bg-[#202123] p-2 md:min-w-[16rem]'>
+    <div className='flex h-screen flex-col space-y-1 bg-[#202123] py-2 md:min-w-[16rem]'>
       {/* new chat */}
-      <div className='flex-1 border-b border-white/20'>
-        <button className='flex w-full items-center space-x-3 rounded-md border border-white/20 p-3 duration-200 ease-out hover:bg-gray-500/10'>
-          <BsPlus className='text-base' />
-          <span className='text-sm'>New Chat</span>
-        </button>
-      </div>
+      <button
+        className='mx-2 flex items-center space-x-3 rounded-md border border-white/20 p-3 duration-200 ease-out hover:bg-gray-500/10'
+        onClick={createNewChat}
+      >
+        <BsPlus className='text-base' />
+        <span className='text-sm'>New Chat</span>
+      </button>
+      {/* chats */}
+      <div className='flex-1 space-y-1 overflow-y-auto border-b border-white/20 px-2'></div>
       {/* clear conversation */}
-      <button className='flex items-center space-x-3 rounded-md p-3 duration-200 hover:bg-gray-500/10'>
+      <button className='mx-2 flex items-center space-x-3 rounded-md p-3 duration-200 hover:bg-gray-500/10'>
         <FiTrash2 className='text-base' />
         <span className='text-sm' onClick={() => signOut()}>
           Clear Conversation
@@ -23,7 +34,7 @@ export default function Sidebar() {
       </button>
       {/* logout */}
       <button
-        className='flex items-center space-x-3 rounded-md p-3 duration-200 hover:bg-gray-500/10'
+        className='mx-2 flex items-center space-x-3 rounded-md p-3 duration-200 hover:bg-gray-500/10'
         onClick={() => signOut()}
       >
         <MdLogout className='text-base' />
